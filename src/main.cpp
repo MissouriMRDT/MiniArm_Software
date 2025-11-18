6a59839bb7c68a55ff32a67cd9f390dfdc0ddc47
Brendan Westley <brendan@westley.us>
2025-11-17 21:18:52 -0600

    Test Basestation-ArduinoJoystick connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+void setup() { Joystick.begin(); }
+
+unsigned int floatToJoystick(float j) {
+  float k = (j + 1.0) / 2.0 * 1024.0;
+  return k < 0 ? 0 : k > 1024 ? 1024 : (unsigned int)k;
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  float x = -0.95;
+  float j2 = -0.5;
+  float j3 = 0;
+  float j4 = 0.5;
+  float pitch = 0.95;
+  Joystick.X(floatToJoystick(x));
+  Joystick.Y(floatToJoystick(j2));
+  Joystick.Z(floatToJoystick(j3));
+  Joystick.Zrotate(floatToJoystick(j4));
+  Joystick.slider(floatToJoystick(pitch));
+  delay(50);
 }
