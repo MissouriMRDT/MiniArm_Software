e2fd89059dcd8df160a6dc4f1d4a2410505f6850
Lucarion45 <lukero45@gmail.com>
2026-01-30 19:01:21 -0600

    Finish Board Test Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,38 @@
 #include <Arduino.h>
 
-void setup() { Joystick.begin(); }
+void setup() {
+    // Joystick.begin();
+    Serial.begin(115200);
+    pinMode(20, INPUT); // Enable
+    pinMode(21, INPUT); // X-AxisA
+    pinMode(22, INPUT); // X-AxisB
+    pinMode(24, INPUT); // Pitch Encoder
+    pinMode(25, INPUT); // J4
+    pinMode(26, INPUT); // J3
+    pinMode(27, INPUT); // J2
+}
 
-unsigned int floatToJoystick(float j) {
-    float k = (j + 1.0) / 2.0 * 1024.0;
-    return k < 0 ? 0 : k > 1024 ? 1024 : (unsigned int)k;
+float floatToJoystick(float eNum) {
+    float jNum = (eNum - 512) / 512;
+    return jNum;
 }
 
 void loop() {
-    float x = -0.95;
-    float j2 = -0.5;
-    float j3 = 0;
-    float j4 = 0.5;
-    float pitch = 0.95;
-    Joystick.X(floatToJoystick(x));
-    Joystick.Y(floatToJoystick(j2));
-    Joystick.Z(floatToJoystick(j3));
-    Joystick.Zrotate(floatToJoystick(j4));
-    Joystick.slider(floatToJoystick(pitch));
-    delay(50);
+    int enable = digitalRead(20);
+    if (enable > 0) {
+        float x = 0.0;
+        float j2 = analogRead(27);
+        float j3 = analogRead(26);
+        float j4 = analogRead(25);
+        float pitch = analogRead(24);
+        // Joystick.X(floatToJoystick(x));
+        // Joystick.Y(floatToJoystick(j2));
+        // Joystick.Z(floatToJoystick(j3));
+        // Joystick.Zrotate(floatToJoystick(j4));
+        // Joystick.slider(floatToJoystick(pitch));
+        Serial.println(floatToJoystick(pitch));
+    } else {
+        Serial.println("DISABLED");
+    }
+    delay(100);
 }
