025d662be22ee4ae7d0592e0fe675a0f7a56aa18
Lucarion45 <lukero45@gmail.com>
2026-02-18 21:17:55 -0600

    Update Software

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 
 void setup() {
-    // Joystick.begin();
-    Serial.begin(115200);
+    Joystick.begin();
     pinMode(20, INPUT); // Enable
     pinMode(21, INPUT); // X-AxisA
     pinMode(22, INPUT); // X-AxisB
@@ -12,27 +11,27 @@ void setup() {
     pinMode(27, INPUT); // J2
 }
 
-float floatToJoystick(float eNum) {
-    float jNum = (eNum - 512) / 512;
-    return jNum;
+float floatToJoystick(float encoderNum) {
+    float joystickNum = (encoderNum - 512) / 512;
+    return joystickNum;
 }
 
 void loop() {
     int enable = digitalRead(20);
     if (enable > 0) {
+
+        // Read Encoder Values
         float x = 0.0;
         float j2 = analogRead(27);
         float j3 = analogRead(26);
         float j4 = analogRead(25);
-        float pitch = analogRead(24);
-        // Joystick.X(floatToJoystick(x));
-        // Joystick.Y(floatToJoystick(j2));
-        // Joystick.Z(floatToJoystick(j3));
-        // Joystick.Zrotate(floatToJoystick(j4));
-        // Joystick.slider(floatToJoystick(pitch));
-        Serial.println(floatToJoystick(pitch));
-    } else {
-        Serial.println("DISABLED");
+        float pitch = analogRead(24); 
+
+        // Converted Joystick Values Sent to Basestation
+        Joystick.X(x);
+        Joystick.Y(j2);
+        Joystick.Z(j3);
+        Joystick.Zrotate(j4);
+        Joystick.slider(pitch);
     }
-    delay(100);
 }
